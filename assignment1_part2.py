40e3bc416ac6a9cd1a752aafc9bdc3d5e78ca699
williamwaffles <41193361+williamarnaiz@users.noreply.github.com>
2021-09-05 22:54:30 -0400

    Update assignment1_part2.py

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -4,8 +4,14 @@ class Book:
         pass
     
     def display(self):
-        pass
+        print(Book)
 
+
+b1 = Book('Of Mice and Men', 'written by John Steinbeck')
+b2 = Book('To Kill a Mockingbird', 'written by Harper Lee')
+
+print(b1.display())
+b2.display()
 
 if __name__ == "__main__":
     pass
