16695a1b5304466761a2712b666d19211a953cad
williamwaffles <41193361+williamarnaiz@users.noreply.github.com>
2021-09-05 23:06:47 -0400

    Update assignment1_part2.py
    Corrected Book class function and corrected object print calls

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,17 +1,17 @@
 
 class Book:
     def __init__(self, author, title):
-        pass
+        self.author = author
+        self.title = title
     
     def display(self):
-        print(Book)
-
+        pass
 
 b1 = Book('Of Mice and Men', 'written by John Steinbeck')
 b2 = Book('To Kill a Mockingbird', 'written by Harper Lee')
 
-print(b1.display())
-b2.display()
+print(b1.author,b1.title)
+print(b2.author,b2.title)
 
 if __name__ == "__main__":
     pass
